bedc84f117dfb92dacceb4c1de76088d2d51dfa9
Cristobal Araneda <64280647+aranedacristobal@users.noreply.github.com>
2024-12-04 20:09:30 +0100

    Update Install-Azure-VPN.ps1

--- Install-Azure-VPN.ps1
+++ Install-Azure-VPN.ps1
@@ -1,9 +1,8 @@
 Start-Transcript (Join-Path $env:TEMP 'Install-Azure-VPN.log')
 
 # Funktion för att kolla om Winget är installerad
-function Check-Winget {
+function Test-WingetInstalled {
     try {
-        # Kolla 
         $winget = Get-Command winget -ErrorAction Stop
         Write-Host "winget är installerad. Version: $($winget.FileVersionInfo.FileVersion)"
         return $true
@@ -28,13 +27,13 @@ function Install-Winget {
     Add-AppxPackage -Path $installerPath
 
     # Steka
-    Remove-Item -Path $installerPath -Force
+    #Remove-Item -Path $installerPath -Force
 
     Write-Host "winget installation klar."
 }
 
 # Exekvera
-if (-not (Check-Winget)) {
+if (-not (Test-WingetInstalled)) {
     Install-Winget
 } else {
     Write-Host "Inget behövs. winget är redan installerad."
@@ -54,7 +53,7 @@ $fullPath = "C:\Users\$username\AppData\Local\Packages\Microsoft.AzureVpn_8wekyb
 $sourceFile = "\\YOURDOMAIN.LOCAL\NETLOGON\Azure-vpn\rasphone.pbk" 
 
 # Exekvera
-    Copy-Item -Path $sourceFile -Destination $fullPath -Force
-    Write-Output "Fil kopierad to $fullPath"
+Copy-Item -Path $sourceFile -Destination $fullPath -Force
+Write-Output "Fil kopierad to $fullPath"
 
 Stop-Transcript
