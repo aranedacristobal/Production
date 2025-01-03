d23f7c179d51ce2de1ec61b8fbd45d41e25d37d1
Cristobal Araneda <64280647+aranedacristobal@users.noreply.github.com>
2025-01-03 23:30:28 +0100

    Update Ninja-build-csv.ps1

--- Ninja/Ninja-automation/List/Ninja-build-csv.ps1
+++ Ninja/Ninja-automation/List/Ninja-build-csv.ps1
@@ -1,4 +1,4 @@
-# Trigga powerkjellskript
+# Trigger power shell script
 
 & 'Ninja\Ninja-automation\List\Locations\Ninja-token.ps1'
 
@@ -8,30 +8,30 @@ Start-Sleep -Seconds 10
 
 Start-Sleep -Seconds 15
 
-# Importera
+# Import
 $csvPath1 = "Ninja\Ninja-automation\List\Locations\locations.csv"  # Change this to your second input CSV file path
 $csvPath2 = "Ninja\Ninja-automation\List\Organisations\organisations.csv"  # Change this to your first input CSV file path
 $outputPath = "Ninja\Ninja-automation\List\output2.csv"  # Change this to your desired output CSV file path
 
-# Läs in
+# Read in
 $data1 = Import-Csv -Path $csvPath1
 $data2 = Import-Csv -Path $csvPath2
 
-# Loopa
+# Loop
 foreach ($record2 in $data2) {
-    # Hitta matcher uppslag i första CSV baserat på organizationId
+    # Find matching records in the first CSV based on organizationId
     $matchingRecord = $data1 | Where-Object { $_.organizationId -eq $record2.organizationid } | Select-Object -First 1
     
-        # Om matcher hittas uppdatera i andra csv
+    # If a match is found, update in the second CSV
     if ($matchingRecord) {
         $record2.id = $matchingRecord.id 
     }
 }
 
-# Tvätta
+# Clean up
 $modifiedData = $data2 | Select-Object name, 
     @{Name='locationid'; Expression={$_.id}},  
     organizationid
 
-# Exportera 
+# Export
 $modifiedData | Export-Csv -Path $outputPath -NoTypeInformation -Delimiter ','
